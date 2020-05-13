56223abe899c898dd9d3a73064385eae97182776
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-05-13 21:31:22 +0600

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,10 @@
+<?php
+
+$server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "ecom";
+$conn = mysqli_connect($server, $db_user, $db_pass, $db_name);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>Zarif's Organic Shop</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand">Zarif's Organic Shop</a>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </nav>
+    <br>
+    <section class="page-section">
+        <h1 class="text-center text-uppercase">Welcome to Zarif's Organic Shop</h1>
+        <br />
+        <div class="container">
+            <div class="row">
+                <?php
+                    include('Config.php');
+                    $sql = "SELECT * FROM `products`";
+                    $query = mysqli_query($conn, $sql);
+                    while($row = mysqli_fetch_assoc($query)){
+                        echo '
+                        <div class="col-sm-4">
+                            <div class="card" style="width: 18rem;">
+                                <img src="'. $row["link"] .'" class="card-img-top" alt="..." style="height: 14rem"/>
+                                <div class="card-body">
+                                    <h5 class="card-title">'.$row["title"].'</h5>
+                                    <p class="card-text"> $'.$row["price"].'</p>
+                                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                                </div>
+                            </div>
+                        </div>
+                        ';
+                    }
+                ?>
+            </div>
+        </div>
+
+    </section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
