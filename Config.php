5f5eca519e1080a47356d6f92d29d44bc81c9dc2
ZarifSoftware <56337492+ZarifSoftware@users.noreply.github.com>
2020-06-20 08:19:54 +0600

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
+$server = "sql101.epizy.com";
 $db_user = "root";
-$db_pass = "";
-$db_name = "ecom";
+$db_pass = "ZZ83GUCFvkJX";
+$db_name = "epiz_26056383_Zstodo";
 $conn = mysqli_connect($server, $db_user, $db_pass, $db_name);
 
 
